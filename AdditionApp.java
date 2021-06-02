52d8e6067016092bc76f136d153246e77a4964a6
Bhavana-Developer <84884327+Bhavana-Developer@users.noreply.github.com>
2021-06-02 16:01:45 +0530

    Add files via upload

---
+++ AdditionApp.java
@@ -0,0 +1,22 @@
+class Addition{
+	
+	/**/
+	 
+	public int add(int num1,int num2){
+		
+		int result=num1+num2;
+		
+		System.out.println("The Addition of the numbers is:"+result);
+		
+		return result;
+} 
+}
+ 
+ class AdditionApp{
+	 public static void main(String args[]){
+		 
+		 Addition addition=new Addition();
+		       addition.add(10,20);
+ }
+ }
+ 
